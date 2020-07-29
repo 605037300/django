13b9892ab164e1c6f98eb5658356583d1424ce81
baojunchen <605037300@qq.com>
2020-07-29 14:07:16 +0800

    meta是建立一个1对1的表关系

--- app01/models.py
+++ app01/models.py
@@ -6,7 +6,8 @@ class Main(models.Model):
     name=models.CharField(max_length=64,default='')
     tracked=models.IntegerField(default=1)
     # 修改默认的表明,class Meta修改元数据
-    class Meta:
+    # 这里两个不一样，meta会建立3张表格，main作为主表，其他作为从表
+    class meta:
         abstract=True
 
 class Mainwhell(Main):
